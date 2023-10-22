ad9166673aa5e439d2a89b709ea822b2e386da2a
Alx Sa <cmyk.student@gmail.com>
2023-10-22 13:53:30 +0000

    dialogs: Refactoring
    Move shared functions between welcome
    and preferences into preferences-dialog-utils

--- app/dialogs/welcome-dialog.c
+++ app/dialogs/welcome-dialog.c
@@ -55,18 +55,9 @@
 
 static GtkWidget * welcome_dialog_new                (Gimp          *gimp,
                                                       GimpConfig    *config);
-static void   prefs_config_notify                    (GObject       *config,
-                                                      GParamSpec    *param_spec,
-                                                      GObject       *config_copy);
-static void   prefs_config_copy_notify               (GObject       *config_copy,
-                                                      GParamSpec    *param_spec,
-                                                      GObject       *config);
 static void   welcome_dialog_response                (GtkWidget     *widget,
                                                       gint           response_id,
                                                       GtkWidget     *dialog);
-static void   welcome_message                        (GtkMessageType type,
-                                                      gboolean       destroy,
-                                                      const gchar   *message);
 static void   welcome_dialog_release_item_activated  (GtkListBox    *listbox,
                                                       GtkListBoxRow *row,
                                                       gpointer       user_data);
@@ -89,18 +80,6 @@ static void   welcome_dialog_create_personalize_page (Gimp           *gimp,
 static void   welcome_dialog_create_creation_page    (Gimp           *gimp,
                                                       GtkWidget      *welcome_dialog,
                                                       GtkWidget      *main_vbox);
-
-/* MOVE TO PREFERENCES_UTIL */
-static void   prefs_gui_config_notify_font_size      (GObject        *config,
-                                                      GParamSpec     *pspec,
-                                                      GtkRange       *range);
-static void   prefs_font_size_value_changed          (GtkRange       *range,
-                                                      GimpGuiConfig  *config);
-static void   prefs_gui_config_notify_icon_size      (GObject        *config,
-                                                      GParamSpec     *pspec,
-                                                      GtkRange       *range);
-static void   prefs_icon_size_value_changed          (GtkRange       *range,
-                                                      GimpGuiConfig  *config);
 
 static GtkWidget *welcome_dialog;
 
@@ -144,83 +123,6 @@ welcome_dialog_create (Gimp *gimp)
                                "gimp-welcome-dialog");
 
   return welcome_dialog;
-}
-
-static void
-prefs_config_notify (GObject    *config,
-                     GParamSpec *param_spec,
-                     GObject    *config_copy)
-{
-  GValue global_value = G_VALUE_INIT;
-  GValue copy_value   = G_VALUE_INIT;
-
-  g_value_init (&global_value, param_spec->value_type);
-  g_value_init (&copy_value,   param_spec->value_type);
-
-  g_object_get_property (config,      param_spec->name, &global_value);
-  g_object_get_property (config_copy, param_spec->name, &copy_value);
-
-  if (g_param_values_cmp (param_spec, &global_value, &copy_value))
-    {
-      g_signal_handlers_block_by_func (config_copy,
-                                       prefs_config_copy_notify,
-                                       config);
-
-      g_object_set_property (config_copy, param_spec->name, &global_value);
-
-      g_signal_handlers_unblock_by_func (config_copy,
-                                         prefs_config_copy_notify,
-                                         config);
-    }
-
-  g_value_unset (&global_value);
-  g_value_unset (&copy_value);
-}
-
-static void
-prefs_config_copy_notify (GObject    *config_copy,
-                          GParamSpec *param_spec,
-                          GObject    *config)
-{
-  GValue copy_value   = G_VALUE_INIT;
-  GValue global_value = G_VALUE_INIT;
-
-  g_value_init (&copy_value,   param_spec->value_type);
-  g_value_init (&global_value, param_spec->value_type);
-
-  g_object_get_property (config_copy, param_spec->name, &copy_value);
-  g_object_get_property (config,      param_spec->name, &global_value);
-
-  if (g_param_values_cmp (param_spec, &copy_value, &global_value))
-    {
-      if (param_spec->flags & GIMP_CONFIG_PARAM_CONFIRM)
-        {
-#ifdef GIMP_CONFIG_DEBUG
-          g_print ("NOT Applying prefs change of '%s' to edit_config "
-                   "because it needs confirmation\n",
-                   param_spec->name);
-#endif
-        }
-      else
-        {
-#ifdef GIMP_CONFIG_DEBUG
-          g_print ("Applying prefs change of '%s' to edit_config\n",
-                   param_spec->name);
-#endif
-          g_signal_handlers_block_by_func (config,
-                                           prefs_config_notify,
-                                           config_copy);
-
-          g_object_set_property (config, param_spec->name, &copy_value);
-
-          g_signal_handlers_unblock_by_func (config,
-                                             prefs_config_notify,
-                                             config_copy);
-        }
-    }
-
-  g_value_unset (&copy_value);
-  g_value_unset (&global_value);
 }
 
 static GtkWidget *
@@ -390,30 +292,12 @@ welcome_dialog_response (GtkWidget *widget,
           g_string_append_printf (string, "\xe2\x80\xa2 %s\n", g_param_spec_get_nick (param_spec));
         }
 
-      welcome_message (GTK_MESSAGE_INFO, FALSE, string->str);
+      prefs_message (dialog, GTK_MESSAGE_INFO, FALSE, string->str);
 
       g_string_free (string, TRUE);
     }
 
   g_list_free (restart_diff);
-
-  gtk_widget_destroy (dialog);
-}
-
-static void
-welcome_message (GtkMessageType  type,
-                 gboolean        destroy_with_parent,
-                 const gchar    *message)
-{
-  GtkWidget *dialog;
-
-  dialog = gtk_message_dialog_new (GTK_WINDOW (welcome_dialog),
-                                   destroy_with_parent ?
-                                   GTK_DIALOG_DESTROY_WITH_PARENT : 0,
-                                   type, GTK_BUTTONS_OK,
-                                   "%s", message);
-
-  gtk_dialog_run (GTK_DIALOG (dialog));
 
   gtk_widget_destroy (dialog);
 }
@@ -1170,70 +1054,4 @@ welcome_size_allocate (GtkWidget     *welcome_dialog,
   gtk_widget_set_visible (image, TRUE);
 
   gtk_window_set_resizable (GTK_WINDOW (welcome_dialog), FALSE);
-}
-
-/* MOVE TO PREFERENCES UTIL */
-static void
-prefs_font_size_value_changed (GtkRange      *range,
-                               GimpGuiConfig *config)
-{
-  gdouble value = gtk_range_get_value (range);
-
-  g_signal_handlers_block_by_func (config,
-                                   G_CALLBACK (prefs_gui_config_notify_font_size),
-                                   range);
-  g_object_set (G_OBJECT (config),
-                "font-relative-size", value / 100.0,
-                NULL);
-  g_signal_handlers_unblock_by_func (config,
-                                     G_CALLBACK (prefs_gui_config_notify_font_size),
-                                     range);
-}
-
-static void
-prefs_gui_config_notify_font_size (GObject    *config,
-                                   GParamSpec *pspec,
-                                   GtkRange   *range)
-{
-  g_signal_handlers_block_by_func (range,
-                                   G_CALLBACK (prefs_font_size_value_changed),
-                                   config);
-  gtk_range_set_value (range,
-                       GIMP_GUI_CONFIG (config)->font_relative_size * 100.0);
-  g_signal_handlers_unblock_by_func (range,
-                                     G_CALLBACK (prefs_font_size_value_changed),
-                                     config);
-}
-
-static void
-prefs_icon_size_value_changed (GtkRange      *range,
-                               GimpGuiConfig *config)
-{
-  gint value = (gint) gtk_range_get_value (range);
-
-  g_signal_handlers_block_by_func (config,
-                                   G_CALLBACK (prefs_gui_config_notify_icon_size),
-                                   range);
-  g_object_set (G_OBJECT (config),
-                "custom-icon-size", (GimpIconSize) value,
-                NULL);
-  g_signal_handlers_unblock_by_func (config,
-                                     G_CALLBACK (prefs_gui_config_notify_icon_size),
-                                     range);
-}
-
-static void
-prefs_gui_config_notify_icon_size (GObject    *config,
-                                   GParamSpec *pspec,
-                                   GtkRange   *range)
-{
-  GimpIconSize size = GIMP_GUI_CONFIG (config)->custom_icon_size;
-
-  g_signal_handlers_block_by_func (range,
-                                   G_CALLBACK (prefs_icon_size_value_changed),
-                                   config);
-  gtk_range_set_value (range, (gdouble) size);
-  g_signal_handlers_unblock_by_func (range,
-                                     G_CALLBACK (prefs_icon_size_value_changed),
-                                     config);
 }
